d6bd12c0efd1f0c61d155ec205eef510d1109de0
tckz <at.tckz@gmail.com>
2012-02-20 19:21:33 +0900

    initial

---
+++ init.rb
@@ -0,0 +1,21 @@
+require 'redmine'
+
+
+Redmine::Plugin.register :redmine_wiki_preview_ext do
+	name 'Wiki preview extension plugin'
+	author 'tckz'
+  description 'Expand wiki preview.'
+	version '0.0.1'
+	requires_redmine :version_or_higher => '1.3.0'
+  url "http://passing.breeze.cc/mt/" if respond_to?(:url)
+	settings :default => {
+			"preview_duration" => "0",
+		}, :partial => 'wiki_preview_ext/settings'
+
+end
+
+# refer it
+WikiPreviewExtHelper::ViewListener
+
+# vim: set ts=2 sw=2 sts=2:
+
